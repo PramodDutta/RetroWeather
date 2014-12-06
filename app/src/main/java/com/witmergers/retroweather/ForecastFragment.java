98e1aece096362a1d2d1ff620fdf8d266ee75a22
Pramod Dutta <pramoddutta_96@yahoo.com>
2014-12-06 17:40:42 +0530

    ADD setting

--- app/src/main/java/com/witmergers/retroweather/ForecastFragment.java
+++ app/src/main/java/com/witmergers/retroweather/ForecastFragment.java
@@ -1,6 +1,7 @@
 package com.witmergers.retroweather;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -11,8 +12,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -91,6 +94,16 @@ public class ForecastFragment extends Fragment {
 
         ListView listView = (ListView)rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getActivity(),mForecastAdapter.getItem(i),Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(),DetailActivity.class);
+                intent.putExtra(Intent.EXTRA_TEXT,mForecastAdapter.getItem(i));
+                startActivity(intent);
+
+            }
+        });
 
 
         return rootView;
